770d5dc9a4f7d03d544ca3a6d9dba6d40f53b4ec
blesswrld <roninfortnite71@gmail.com>
2025-03-28 22:07:32 +0300

    use Redux-Persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,23 @@
 import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
+const persistConfig = {
+    key: "root",
+    storage,
+    // так же можно создать ключи для сохранения определенных элементов
+    // whitelist: ["filters"], то что сохраняем
+    // blacklist: ["positions"], то что не хотим сохранить
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export { store };
+export const persister = persistStore(store);
